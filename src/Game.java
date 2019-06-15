4bc4a2ba4d675227756707d626dce4a49a9e4de6
RonFMitchell <ron.mitchell810@gmail.com>
2019-06-14 21:01:40 -0400

    Hey Will, here is some more battle system stuff.

--- src/Game.java
+++ src/Game.java
@@ -13,9 +13,35 @@ class Game {
 
     while(battleOngoing) {
       int move = 0;
+
       Scanner sc = new Scanner(System.in);
       move = sc.nextInt();
-      System.out.print("What is your move: ");
+
+      while(true)
+        System.out.print("What is your move? 1: Attack, 2: Defend, 3: Item, 4: Run: ");
+
+        switch(move) {
+          case 1:
+            player.attack();
+            break;
+          case 2:
+            player.defend();
+            break;
+          case 3:
+            player.item();
+            break;
+          case 4:
+            player.run();
+            break;
+          default:
+            System.out.println("Invalid Response.  Try Again");
+            break;
+
+      if(enemy.tempHealth == 0){
+        System.out.println("You have defeated the :" + e.getName());
+        break;
+      }
+      }
     }
   }
 }
